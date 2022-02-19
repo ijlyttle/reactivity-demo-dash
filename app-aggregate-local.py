30699a1bd88abdf3b64bbace75f75e309d263135
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-02-19 11:57:15 -0600

    simplify app

--- app-aggregate-local.py
+++ app-aggregate-local.py
@@ -10,18 +10,14 @@ import dash_bootstrap_components as dbc
 
 import pandas as pd
 from palmerpenguins import load_penguins
-import base64
-import io
 
-from helpers.aggregate import aggregate_df
+from helpers.aggregate import aggregate_df, agg_function_choices
 from helpers.cols import cols_choice, cols_header
 
 app = dash.Dash(
     __name__, 
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
-
-agg_function_choices = ['mean', 'min', 'max']
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -43,20 +39,6 @@ app.layout = html.Div(
                     className='col-sm-4',
                     children=[
                         dbc.Card([
-                            dbc.CardHeader('Input data'),
-                            dbc.CardBody([
-                                dcc.Upload(
-                                    dbc.Button('Upload CSV File', className='btn btn-secondary'),
-                                    id='upload-inp'
-                                ),      
-                                html.P(
-                                    html.I('No file loaded, using penguins as default', id='upload-status')
-                                ),
-                                dbc.Button('Download CSV File', className='btn btn-secondary', id='download-btn-inp'),
-                                dcc.Download(id='download-inp')
-                            ])
-                        ]),
-                        dbc.Card([
                             dbc.CardHeader('Aggregation'),
                             dbc.CardBody([
                                 dbc.Label('Grouping columns'),
@@ -71,13 +53,6 @@ app.layout = html.Div(
                                 ),
                                 html.Hr(),
                                 dbc.Button('Submit', className='btn btn-secondary', id='button-agg')
-                            ])
-                        ]),
-                        dbc.Card([
-                            dbc.CardHeader('Aggregated data'),
-                            dbc.CardBody([
-                                dbc.Button('Download CSV File', className='btn btn-secondary', id='download-btn-agg'),
-                                dcc.Download(id='download-agg')
                             ])
                         ])
                     ]
@@ -97,7 +72,7 @@ app.layout = html.Div(
                             id='table-agg',
                             page_size=10,
                             sort_action='native'
-                        ),
+                        )
                     ]
                 )
             ]
@@ -105,59 +80,24 @@ app.layout = html.Div(
     ]
 ) 
 
-@app.callback(Output('upload-status', 'children'),
-              Input('upload-inp', 'filename'),
-              prevent_initial_call=True)
-def update_input_file_name(name):
-    return name
-
-@app.callback(Output('inp', 'data'),
-              Input('upload-inp', 'contents'),
-              prevent_initial_call=True)
-def parse_input_file_contents(contents):
-
-    try:
-        # decode content to DataFrame
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-
-        df = pd.read_csv(
-            io.StringIO(decoded.decode('utf-8'))  
-        )
-    except Exception as e:
-        print(e)
-        df = pd.DataFrame()
-
-    # data stored in DOM as records
-    data_records = df.to_dict('records')
-
-    return data_records
-
-@app.callback(
-    Output('download-inp', 'data'),
-    Input('download-btn-inp', 'n_clicks'),
-    State('inp', 'data'),
-    prevent_initial_call=True,
-)
-def download_inp(n_clicks, data_records):
-    df = pd.DataFrame.from_dict(data_records)
-    return dcc.send_data_frame(df.to_csv, 'download-inp.csv', index=False)
-
 @app.callback(Output('table-inp', 'columns'),
               Output('table-inp', 'data'),
               Input('inp', 'data'))
 def update_table_inp(data_records):
      return cols_header(data_records), data_records   
 
+
 @app.callback(Output('cols-group', 'options'),
               Input('inp', 'data'))
 def update_cols_group(data_records):
     return cols_choice(data_records, 'object')
 
+
 @app.callback(Output('cols-agg', 'options'),
               Input('inp', 'data'))
 def update_cols_agg(data_records):
     return cols_choice(data_records, 'number')
+
 
 @app.callback(Output('agg', 'data'),
               Input('button-agg', 'n_clicks'),
@@ -176,21 +116,13 @@ def aggregate(n_clicks, data_records, cols_group, cols_agg, func_agg):
     # serialize DataFrame
     return df_new.to_dict('records')
 
+
 @app.callback(Output('table-agg', 'columns'),
               Output('table-agg', 'data'),
               Input('agg', 'data'))
 def update_table_agg(data_records):
     return cols_header(data_records), data_records
 
-@app.callback(
-    Output('download-agg', 'data'),
-    Input('download-btn-agg', 'n_clicks'),
-    State('agg', 'data'),
-    prevent_initial_call=True,
-)
-def download_agg(n_clicks, data_records):
-    df = pd.DataFrame.from_dict(data_records)
-    return dcc.send_data_frame(df.to_csv, 'download-agg.csv', index=False)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- helpers/aggregate.py
+++ helpers/aggregate.py
@@ -1,17 +1,23 @@
 import pandas as pd
 
-def aggregate_df (df, cols_group, cols_agg, func_agg):
+agg_function_choices = ['mean', 'min', 'max']
+
+def aggregate_df (df, cols_group, cols_agg, func_agg,
+                  str_fn_choices = agg_function_choices):
+    
+    if not func_agg in str_fn_choices:
+        raise AssertionError(f"{func_agg} not a legal function-choice")
+  
     if (not cols_group is None):
         df = df.groupby(cols_group)
 
     if (cols_agg is None or len(cols_agg) == 0):
         return []
-    
+
     dict_agg = {}
     for col in cols_agg:
         dict_agg[col] = func_agg
     
-    # aggregate DataFrame
     df = df.agg(dict_agg).reset_index()
 
     return df
